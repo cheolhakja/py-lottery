d7acf1bac1177f1bb2173fd2a944178f3ad79b4c
cheolhakja <rebegin15@gmail.com>
2024-02-28 00:22:49 +0900

    test changing timezone to seoul

--- time_limit.py
+++ time_limit.py
@@ -1,10 +1,12 @@
 result = [(1, "nunez", "2023-12-12 23:11:45"),(2, "salah", "2024-01-01 00:00:00"),(3, "virgil", "2024-01-01 00:00:00"),(4, "robo", "2024-01-01 00:00:00")]
 
 from datetime import datetime
+from pytz import timezone
 
-now = datetime.now()
+now = datetime.now(timezone('Asia/Seoul'))
 
 last_time_buy = result[0][2] #"2023-12-12 23:11:45"
 
 
 print(last_time_buy < now.strftime('%Y-%m-%d %H:%M:%S')) #True
+print(now.strftime('%Y-%m-%d %H:%M:%S'))
